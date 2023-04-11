ba66de97f79384e235cbeda958de0d6cb36a59b7
Temi Owolabi <65958151+temiowolabi@users.noreply.github.com>
2023-04-11 14:46:16 +0100

    'Deploy'

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(express.static(path.join(__dirname, '../../main/findacook-app/build')))
 // res.sendFile(path.join(__dirname, '../../main/findacook-app/build/index.html'))
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname,  '../../main/findacook-app/build/index.html'));
+    res.sendFile(path.join(__dirname,  '../main/findacook-app/build/index.html'));
   });
 
 app.use(cors());
